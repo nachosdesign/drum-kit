f5d6182717f62e498a8f70ba2aa65fb3c2714132
Nacho Allende <iallende@Rebellious-Amish-L.local>
2020-08-03 11:05:16 +0200

    some changes

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-hello
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        this.style.color = "blue";
+
+    });
+
+}
+
+//    new Audio('sounds/tom-1.mp3').play();
